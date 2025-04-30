da7c0245b01dcd3f277c5638d218f972b17f1190
Marosi Csaba <marosicsaba91@gmail.com>
2025-04-30 19:38:09 +0200

    Initial commit

---
+++ Assets/BubbleShooterKit/Scripts/Common/SaveSystem.cs
@@ -0,0 +1,21 @@
+﻿enum SaveIntKey
+{
+
+}
+enum SaveBoolKey
+{
+
+}
+
+static class SaveSystem
+{
+    public static int GetInt(SaveIntKey key)
+    {
+        return 0;
+    }
+
+    public static void SetInt(SaveIntKey key, int value)
+    {
+
+    }
+}
---
+++ Assets/Scripts/TestMenu.cs
@@ -0,0 +1,39 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestMenu : MonoBehaviour
+{
+    [SerializeField] Button fullScreenButton;
+    [SerializeField] Button changeResolutionButton;
+    [SerializeField] TMP_Text infoText; 
+
+    void Awake()
+    {
+        fullScreenButton.onClick.AddListener(ChangeFullScreenMode);
+        changeResolutionButton.onClick.AddListener(ChangeResolution);
+    }
+
+    void ChangeFullScreenMode()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+    void ChangeResolution()
+    {
+        //Switch between 1920x1080 and 1280x720
+        if (Screen.currentResolution.height == 1000)
+        {
+            Screen.SetResolution(1000, 2000, Screen.fullScreen);
+        }
+        else
+        {
+            Screen.SetResolution(1000, 1000, Screen.fullScreen);
+        }
+
+    }
+
+    void Update()
+    {
+        infoText.text = Screen.currentResolution.ToString();
+    }
+}
